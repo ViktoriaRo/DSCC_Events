fb4d416e2493e30c29f983770f458dd6104b955f
victoria <vica.rotaru97@gmail.com>
2018-11-19 11:41:42 +0300

    signIn/signOut links

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,18 +5,15 @@ import SignedOutLinks from './SignedOutLinks';
 import { connect } from 'react-redux';
 
 const Navbar = (props) => {
-    // const { auth } = props;
+    const { auth } = props;
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to="/" className="brand-logo">Events</Link>
-                <SignedInLinks />
-                <SignedOutLinks />
-
+                { links }
             </div>
-
         </nav>
-
     )
 }
 
