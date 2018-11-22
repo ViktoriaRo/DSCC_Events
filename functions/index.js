a3d1b3339884b4a3a9509e582772244b946d1cd3
victoria <vica.rotaru97@gmail.com>
2018-11-22 21:59:19 +0300

    add notification and sort them

--- functions/index.js
+++ functions/index.js
@@ -24,3 +24,17 @@ exports.eventCreated = functions.firestore
 
         return createNotification(notification);
     })
+
+exports.userJoined = functions.auth.user()
+    .onCreate(user => {
+        return admin.firestore().collection('users')
+        .doc(user.uid).get().then(doc => {
+            const newUser = doc.data();
+            const notification = {
+                content: 'Joined the party',
+                user: `${newUser.authorFirstName} ${newUser.authorLastName}`,
+                time: admin.firestore.FieldValue.serverTimestamp().toDate()
+            }
+            return createNotification(notification);
+        })
+    })
