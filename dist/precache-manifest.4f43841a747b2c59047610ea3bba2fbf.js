437d9f96957952b62a51e1b83c3e92a58e78d527
victoria <vica.rotaru97@gmail.com>
2018-11-22 22:56:15 +0300

    host on aws and firebase

---
+++ dist/precache-manifest.4f43841a747b2c59047610ea3bba2fbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4b0ac589825cefcbdf51",
+    "url": "/static/js/main.4b0ac589.chunk.js"
+  },
+  {
+    "revision": "e153aa503529033ffdfa",
+    "url": "/static/js/1.e153aa50.chunk.js"
+  },
+  {
+    "revision": "4b0ac589825cefcbdf51",
+    "url": "/static/css/main.8bd00182.chunk.css"
+  },
+  {
+    "revision": "aca4e8766c6e01c16da278f67fd3df4f",
+    "url": "/index.html"
+  }
+];
